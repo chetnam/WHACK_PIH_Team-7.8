b9ae9e5b6dc238644838cee3178fc618e4d3e10e
hannahtwiggsmith <htwiggsmith@gmail.com>
2016-10-01 19:19:51 -0400

    more vis stuff

--- vis/test.js
+++ vis/test.js
@@ -1,19 +1,47 @@
 
+// set up our data series with 50 random data points
 
+var seriesData = [ [], [] ];
+var random = new Rickshaw.Fixtures.RandomData(500);
+
+for (var i = 0; i < 500; i++) {
+	random.addData(seriesData);
+}
+
+console.log(seriesData);
+
+// instantiate our graph!
 
 var graph = new Rickshaw.Graph( {
-	element: document.querySelector("#chart"),
+	element: document.getElementById("chart"),
+	width: 960,
+	height: 500,
 	renderer: 'scatterplot',
-	stroke: true,
-	series: [{
-		data: [ { x: 0, y: 40 },
-            { x: 1, y: 49 },
-            { x: 2, y: 50 },
-            { x: 3, y: 60 },
-            { x: 4, y: 50 },
-            { x: 5, y: 80 } ] ,
-		color: 'steelblue'
-	}]
+	series: [
+		{
+			color: "#ff9030",
+			data: seriesData[0],
+		}, {
+			color: "#4c4cff",
+			data: seriesData[1],
+		}
+	]
 } );
 
+var xAxis = new Rickshaw.Graph.Axis.Time({
+    graph: graph
+});
+
+var yAxis = new Rickshaw.Graph.Axis.Y({
+    graph: graph,
+    tickFormat: Rickshaw.Fixtures.Number.formatKMBT
+});
+
+
+graph.renderer.dotSize = 6;
+
+new Rickshaw.Graph.HoverDetail({ graph: graph });
+
 graph.render();
+xAxis.render();
+yAxis.render();
