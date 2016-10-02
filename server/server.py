3a8c2af8d45e43dbe894877f04df6945106d9bb4
Samantha Voigt <samanthalvoigt@gmail.com>
2016-10-02 03:38:20 +0000

    sms endpoint updates

--- server/server.py
+++ server/server.py
@@ -11,33 +11,47 @@ import json
 from datetime import date
 from datetime import timedelta
 
+lastMessage = None
+
 @app.route("/")
 def index():
 
     results = db.select_one()
 
-    return render_template("index.html")
+    return render_template("index.html", lastMessage=lastMessage)
 
 @app.route("/sms", methods=['GET', 'POST'])
 def incoming_sms():
     """Send a dynamic reply to an incoming text message"""
     # Get the message the user sent our Twilio number
-    body = request.values.get('Body', None)
+    
+    print request.method
 
-    # Start our TwiML response
-    resp = twiml.Response()
 
-    # Determine the right reply for this message
-    if body == 'hello':
-        resp.message("Hi!!")
-        resp.sms("Hi!")
-    elif body == 'bye':
-        resp.message("Goodbye")
-    else:
-        resp.message("  asdgasdg ")
-        resp.sms("YO")
+    if request.method == 'POST':
 
-    return str(resp)
+        print request.values
+
+        body = request.values.get('Body', None)
+
+        # Start our TwiML response
+        resp = twiml.Response()
+
+        # Determine the right reply for this message
+        if body == 'hello':
+            resp.message("Hi!!")
+            resp.sms("Hi!")
+        elif body == 'bye':
+            resp.message("Goodbye")
+        else:
+            resp.message("  asdgasdg ")
+            resp.sms("YO")
+
+        global lastMessage
+        lastMessage = body
+
+        return str(resp)
+    return "yooooooo this is nothing"
 
 if __name__ == "__main__":
     app.run('0.0.0.0')
